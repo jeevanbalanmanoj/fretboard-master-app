ef648ecbcfc4fadb6d6093835e648e0d2a3fcbe3
jeevan B Manoj <jeevanbmanoj@gmail.com>
2020-08-28 20:58:12 +0530

    Updates

---
+++ precache-manifest.80e314cf7a9ea904938afde46a7e3859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f920f3db6801dc8e9236727e2d828a36",
+    "url": "/fretboard-master-app/index.html"
+  },
+  {
+    "revision": "ba2a5c0c5c59d02245c9",
+    "url": "/fretboard-master-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "770e41c682e1b9f03503",
+    "url": "/fretboard-master-app/static/js/2.4ea800f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fretboard-master-app/static/js/2.4ea800f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2a5c0c5c59d02245c9",
+    "url": "/fretboard-master-app/static/js/main.2c7be7f8.chunk.js"
+  },
+  {
+    "revision": "43aa320f73a179be627d",
+    "url": "/fretboard-master-app/static/js/runtime-main.07e8e3b3.js"
+  }
+]);
